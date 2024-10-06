2c27e0c2fa130b8e5d8b6d5a4390b5add2ae6a73
Alex Delgado <101017492+faelion@users.noreply.github.com>
2024-10-06 20:44:47 +0200

    jump upgraded & anim adjusted

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,20 +5,52 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float force;
+    [SerializeField] private float force = 1.5f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float maxRotation = 20f;
+    [SerializeField] private float minRotation = 290f;
     [SerializeField] private float yBound;
+    private InputController controls;
+    void Awake()
+    {
+        controls = new InputController();
+    }
+
+    private void OnEnable()
+    {
+        controls.Bird.touch.started += ctx => Flap();
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space) && rb.position.y < yBound)
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0,0, rb.velocity.y * rotationSpeed);
+
+        if (transform.rotation.eulerAngles.z > maxRotation && transform.rotation.eulerAngles.z < 90)
         {
-            Flap();
+            transform.rotation = Quaternion.Euler(0, 0, maxRotation);
+        }
+
+        if (transform.rotation.eulerAngles.z < minRotation && transform.rotation.eulerAngles.z > 180)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, minRotation);
         }
     }
 
     private void Flap()
     {
-        rb.velocity = Vector2.zero;
-        rb.AddForce(Vector2.up * force);
+        if (rb.position.y < yBound)
+        {
+            rb.velocity = Vector2.up * force;
+        }
     }
 }
